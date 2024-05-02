d1956303ea7a3914b40fb6f9b45fe588bfddb3f0
Marta Zazlak <marta.zazlak@hotmail.com>
2024-05-02 15:19:16 +0200

    starter

---
+++ src/app/texts.ts
@@ -0,0 +1,66 @@
+export const TEXTS = {
+  appName: 'Simple Character Doc',
+
+  jumbotron: {
+    title: 'Hello there!',
+    subtitle: "Let's create your character description!",
+    note: "We know, you always wanted a nice background character document. No worries, we're on the same site!",
+  },
+
+  form: {
+    avatar: {
+      title: 'Upload your avatar',
+      subtitle: "We're so excited to see your new character!",
+      note: 'Upload your image here!',
+      upload: 'Edit',
+
+      fields: {
+        greyScale: 'Grey scale',
+        Zoom: 'Zoom',
+        Brightness: 'Brightness',
+      },
+    },
+    basicInfo: {
+      title: 'Provide some info',
+      subtitle: 'Who is your character? What about the name?',
+      note: "It's so important to know these basic information!",
+
+      fields: {
+        name: 'What is they name?',
+        age: 'How old are they?',
+        race: 'What is they race?',
+        place: 'What are they come from?',
+        todo1: 'TO DO 1',
+        todo2: 'TO DO 2',
+      },
+      errors: {
+        error1: 'todo error 1',
+      },
+    },
+    background: {
+      title: 'Describe your background',
+      subtitle: 'What would you like to share abut your character?',
+      note: 'Oh, oh, a final stage! Remember to use an epic description!',
+    },
+    theme: {
+      title: 'Select your document theme',
+      subtitle: 'Which theme is your favorite one?',
+      note: 'Which one do you prefer? A green, floral spring or maybe a simple paper notes? Decision is yours!',
+
+      fields: {
+        spring: 'Floral spring',
+        sky: 'Starring sky',
+        notes: 'Paper notes',
+      },
+    },
+  },
+  download: {
+    action: 'Download PDF',
+  },
+  footer: {
+    repo: 'See my public repositories!',
+    contact: 'Stay in touch!',
+    projects: 'See my other projects!',
+    author: 'NdV66 (Marta Zażlak)',
+  },
+};
