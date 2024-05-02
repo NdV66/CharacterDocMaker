74d13fecd64cd2d1142b789d82c647be53131461
Marta Zazlak <marta.zazlak@hotmail.com>
2024-05-02 22:28:53 +0200

    scss

--- src/app/translations/translations.module.ts
+++ src/app/translations/translations.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslationsService } from './translations.service';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [],
+  providers: [],
 })
 export class TranslationsModule {}
---
+++ src/app/translations/translations.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { TEXTS as plPl } from './pl-PL';
+
+enum ELanguages {
+  'PL' = 'pl-PL',
+}
+
+const DEFAULT_LANG = ELanguages.PL;
+const translations = new Map([[ELanguages.PL, plPl]]);
+
+@Pipe({
+  name: 'translations',
+  standalone: true,
+})
+export class TranslationsPipe implements PipeTransform {
+  transform(value: string, language = DEFAULT_LANG): string {
+    const translationsToSearch = translations.get(language);
+    if (!value || !translationsToSearch) return value;
+    const keys = value.split('.');
+    let result: any = translationsToSearch;
+    for (let key of keys) {
+      result = result[key];
+      if (!result) break;
+    }
+    return result;
+  }
+}
