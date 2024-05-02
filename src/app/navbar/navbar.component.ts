382a630e2f0ff79d29eb60dbb12529fe3454c537
Marta Zazlak <marta.zazlak@hotmail.com>
2024-05-02 23:28:35 +0200

    backup

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 @NgModule({
   declarations: [],
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, MatSlideToggleModule],
   exports: [NavbarComponent],
 })
 export class NavbarModule {}
