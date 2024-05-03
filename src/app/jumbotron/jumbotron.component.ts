0fd69ffe2ac26740ecd37db797a657207db54a59
Marta Zazlak <marta.zazlak@hotmail.com>
2024-05-03 15:29:06 +0200

    styles

--- src/app/jumbotron/jumbotron.component.ts
+++ src/app/jumbotron/jumbotron.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslationsPipe } from '../translations/translations.pipe';
+import { NoteComponent } from '../elements/note/note.component';
 
 @Component({
   selector: 'app-jumbotron',
   standalone: true,
-  imports: [TranslationsPipe],
+  imports: [TranslationsPipe, NoteComponent],
   templateUrl: './jumbotron.component.html',
   styleUrl: './jumbotron.component.scss',
 })
