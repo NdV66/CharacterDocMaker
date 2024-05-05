d283d298c74daa798fa060c50337c297ce8f4e45
Marta Zazlak <marta.zazlak@hotmail.com>
2024-05-05 15:00:07 +0200

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,9 @@ import { RouterOutlet } from '@angular/router';
 
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { DocumentThemePanelComponent } from './character-form/document-theme-panel/document-theme-panel.component';
+import { DocumentThemePanelComponent } from './document-panel/document-theme-panel/document-theme-panel.component';
 import { FooterComponent } from './footer/footer.component';
-import { CharacterFormPanelComponent } from './character-form/character-form-panel.component';
+import { CharacterFormPanelComponent } from './character-form-panel/character-form-panel.component';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/document-panel/document-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './document-panel.component.html',
+  styleUrl: './document-panel.component.scss'
+})
+export class DocumentPanelComponent {
+
+}
