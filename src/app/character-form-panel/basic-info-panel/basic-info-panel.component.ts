aee86ec1b33c7e1751f0f50f70457781ab8be353
Marta Zazlak <marta.zazlak@hotmail.com>
2024-05-05 16:17:57 +0200

    backup

--- src/app/character-form-panel/basic-info-panel/basic-info-panel.component.ts
+++ src/app/character-form-panel/basic-info-panel/basic-info-panel.component.ts
@@ -2,22 +2,23 @@ import { Component } from '@angular/core';
 import { TranslationsPipe } from '../../translations/translations.pipe';
 import { TitleComponent } from '../../elements/title/title.component';
 import { BasicInfoPanelService } from './basic-info-panel.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-info-panel',
   standalone: true,
-  imports: [TranslationsPipe, TitleComponent],
+  imports: [TranslationsPipe, TitleComponent, FormsModule],
   templateUrl: './basic-info-panel.component.html',
   styleUrl: './basic-info-panel.component.scss',
 })
 export class BasicInfoPanelComponent {
-  public characterName = '';
+  public characterName = 'Irmo here';
 
   constructor(private _basicInfoPanelService: BasicInfoPanelService) {}
 
-  public onChangeName() {
+  public onChangeName(event: Event) {
     this._basicInfoPanelService.name = 'irmo';
-    console.log('!!!!');
+    console.log('!!!', event);
   }
 
   ngOnInit(): void {
