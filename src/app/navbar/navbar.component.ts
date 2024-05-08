20f91081ad478d25db997f422b7aa4b248c88995
Marta Zazlak <marta.zazlak@hotmail.com>
2024-05-08 19:06:00 +0200

    Unstall material, its a sh*t lib

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { TranslationsPipe } from '../translations/translations.pipe';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatToolbarModule, TranslationsPipe],
+  imports: [TranslationsPipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
