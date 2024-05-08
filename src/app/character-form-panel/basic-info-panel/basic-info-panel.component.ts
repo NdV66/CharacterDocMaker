a54a192f0c0fa403afe503ade7b3fabb95ec4141
Marta Zazlak <marta.zazlak@hotmail.com>
2024-05-08 20:06:12 +0200

    backup

--- src/app/character-form-panel/basic-info-panel/basic-info-panel.component.ts
+++ src/app/character-form-panel/basic-info-panel/basic-info-panel.component.ts
@@ -4,16 +4,31 @@ import { TitleComponent } from '../../elements/title/title.component';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { NgForOf } from '@angular/common';
 import { CharacterFormService } from '../character-form.service';
+import { NoteComponent } from '../../elements/note/note.component';
 
 @Component({
   selector: 'app-basic-info-panel',
   standalone: true,
-  imports: [TranslationsPipe, TitleComponent, NgForOf, ReactiveFormsModule],
+  imports: [
+    TranslationsPipe,
+    TitleComponent,
+    NgForOf,
+    ReactiveFormsModule,
+    NoteComponent,
+  ],
   templateUrl: './basic-info-panel.component.html',
   styleUrl: './basic-info-panel.component.scss',
 })
 export class BasicInfoPanelComponent {
   readonly form: FormGroup = new FormGroup({});
+  readonly fields = [
+    { name: 'name' },
+    { name: 'race' },
+    { name: 'nature' },
+    { name: 'age' },
+    { name: 'origin' },
+    { name: 'live' },
+  ];
 
   constructor(service: CharacterFormService) {
     this.form = service.form;
