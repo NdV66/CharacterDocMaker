70d8c05fa480f7a931e8d867f2273b39039f252e
Marta Zazlak <marta.zazlak@hotmail.com>
2024-05-18 12:14:23 +0200

    refactor

--- src/app/character-form-panel/avatar-panel/avatar-panel.component.ts
+++ src/app/character-form-panel/avatar-panel/avatar-panel.component.ts
@@ -3,11 +3,11 @@ import { TranslationsPipe } from '../../translations/translations.pipe';
 import { PanelComponent } from '../../elements/panel/panel.component';
 import { NoteComponent } from '../../elements/note/note.component';
 import { AvatarFiltersComponent } from './avatar-filters/avatar-filters.component';
-import { UploadImageComponent } from './upload-image/upload-image.component';
 import { FormGroup } from '@angular/forms';
 import { CharacterFormService } from '../../services';
 import { fromEvent } from 'rxjs';
 import { ImageSnippetDto } from '../../models';
+import { UploadImageComponent } from '../../elements/upload-image/upload-image.component';
 
 @Component({
   selector: 'app-avatar-panel',
---
+++ src/app/elements/upload-image/upload-image.component.ts
@@ -0,0 +1,44 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload-image.component.html',
+  styleUrl: './upload-image.component.scss',
+})
+export class UploadImageComponent {
+  @Input() imageUrl = '';
+  @Input() greyScale = 0;
+  @Input() brightness = 0;
+  @Input() zoom = 100;
+  @Input() isLoading = false;
+  @Input() isError = false;
+  @Input() uploadText = '';
+  @Input() loadingText = '';
+
+  @Output('onProcessAvatarEvent') readonly _onProcessAvatarEventEmitter =
+    new EventEmitter<any>();
+
+  private get _greyScaleAsNonPercent() {
+    return this.greyScale / 100;
+  }
+
+  private get _brightnessAsNonPercent() {
+    return this.brightness / 100;
+  }
+
+  get imageInlineStyles() {
+    return {
+      filter: `grayscale(${this._greyScaleAsNonPercent}) brightness(${this._brightnessAsNonPercent})`,
+      backgroundImage: `url(${this.imageUrl})`,
+      backgroundSize: `${this.zoom}% ${this.zoom}%`,
+    };
+  }
+
+  processFile(imageInput: any) {
+    if (this.isLoading) return;
+    this.isLoading = true;
+    this._onProcessAvatarEventEmitter.emit(imageInput.files[0]);
+  }
+}
