b065d358d7df563e80b9435a7454f5f8456c39c7
Marta Zazlak <marta.zazlak@hotmail.com>
2024-05-18 10:31:43 +0200

    refactor, start with error color

--- src/app/character-form-panel/description-panel/description-panel.component.ts
+++ src/app/character-form-panel/description-panel/description-panel.component.ts
@@ -4,15 +4,23 @@ import { PanelComponent } from '../../elements/panel/panel.component';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CharacterFormService } from '../../services/character-form.service';
 import { NoteComponent } from '../../elements/note/note.component';
+import { LabelComponent } from '../../elements/label/label.component';
 
 @Component({
   selector: 'app-description-panel',
   standalone: true,
-  imports: [TranslationsPipe, PanelComponent, ReactiveFormsModule, NoteComponent],
+  imports: [
+    TranslationsPipe,
+    PanelComponent,
+    ReactiveFormsModule,
+    NoteComponent,
+    LabelComponent,
+  ],
   templateUrl: './description-panel.component.html',
   styleUrl: './description-panel.component.scss',
 })
 export class DescriptionPanelComponent {
+  isError = true;
   readonly form: FormGroup = new FormGroup({});
 
   constructor(service: CharacterFormService) {
