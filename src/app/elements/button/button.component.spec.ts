ee4b3336ff4ada7063294eb0408f643e7d4478a3
Marta Zazlak <marta.zazlak@hotmail.com>
2024-06-15 13:19:31 +0200

    tests

--- src/app/elements/button/button.component.spec.ts
+++ src/app/elements/button/button.component.spec.ts
@@ -10,6 +10,54 @@ describe('ButtonComponent', () => {
     // When
     await render(ButtonComponent, { componentProperties: { text: textMock } });
     // Then
-    expect(screen.getByText(textMock)).toBeTruthy();
+    const button = screen.getByText(textMock);
+    expect(button).toBeInTheDocument();
+    expect(button).not.toBeDisabled();
+  });
+
+  it('Should be disabled', async () => {
+    // Given
+    // When
+    await render(ButtonComponent, {
+      componentProperties: { text: textMock, isDisabled: true },
+    });
+    // Then
+    const button = screen.getByText(textMock);
+    expect(button).toBeDisabled();
+  });
+
+  it('Should not be clickable when disabled', async () => {
+    // Given
+    const onClickMock = jest.fn();
+    await render(ButtonComponent, {
+      componentProperties: {
+        text: textMock,
+        isDisabled: true,
+        onClick: onClickMock,
+      },
+    });
+    const button = screen.getByText(textMock);
+    // When
+    await userEvent.click(button);
+    // Then
+    expect(button).toBeDisabled();
+    expect(onClickMock).not.toHaveBeenCalled();
+  });
+
+  it('Should be clickable when not disabled', async () => {
+    // Given
+    const onClickMock = jest.fn();
+    await render(ButtonComponent, {
+      componentProperties: {
+        text: textMock,
+        onClick: onClickMock,
+      },
+    });
+    const button = screen.getByText(textMock);
+    // When
+    await userEvent.click(button);
+    // Then
+    expect(button).not.toBeDisabled();
+    expect(onClickMock).toHaveBeenCalledTimes(1);
   });
 });
