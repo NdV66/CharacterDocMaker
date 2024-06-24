600d269d8b1540d37de3491be1bf3ef129f65ca1
Marta Zazlak <marta.zazlak@hotmail.com>
2024-06-24 16:53:49 +0200

    tests

--- src/app/elements/field-error/field-error.component.spec.ts
+++ src/app/elements/field-error/field-error.component.spec.ts
@@ -2,6 +2,7 @@ import { screen, render } from '@testing-library/angular';
 import { FieldErrorComponent } from './field-error.component';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { TEXTS } from '@translations/en-EN';
+import * as formUtils from '@utils/form';
 
 const fb = new FormBuilder();
 const formGroupMock = fb.group({
@@ -22,21 +23,21 @@ describe('ButtonComponent', () => {
 
   it('Should not render any error', async () => {
     // Given
+    jest.spyOn(formUtils, 'isFormControlInvalid').mockReturnValue(false);
     // When
     await renderElement();
     // Then
-    const element = screen.getByText('This field is required');
-    expect(element).toBeVisible();
+    expect(screen.queryByText(TEXTS.form.errors.required)).toBeNull();
   });
 
   it('Should not render any error (even when touched)', async () => {
     // Given
+    jest.spyOn(formUtils, 'isFormControlInvalid').mockReturnValue(false);
     formControlItem.markAllAsTouched();
     // When
     await renderElement();
     // Then
-    const element = screen.getByText('This field is required');
-    expect(element).toBeVisible();
+    expect(screen.queryByText(TEXTS.form.errors.required)).toBeNull();
   });
 
   const cases = [
@@ -48,6 +49,7 @@ describe('ButtonComponent', () => {
 
   it.each(cases)(`Should show $1 error`, async ({ error, text }) => {
     // Given
+    jest.spyOn(formUtils, 'isFormControlInvalid').mockReturnValue(true);
     formControlItem.setErrors({ [error]: true });
     formControlItem.markAllAsTouched();
     // When
