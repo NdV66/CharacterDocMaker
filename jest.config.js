e1b8331bab5276b66220d91f729043708f7a3c6b
Marta Zazlak <marta.zazlak@hotmail.com>
2024-06-27 15:26:23 +0200

    jest config setup

--- jest.config.js
+++ jest.config.js
@@ -3,16 +3,18 @@ module.exports = {
   globalSetup: "jest-preset-angular/global-setup",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
   transform: {
-    "^.+\\.(ts|js|html)$": "jest-preset-angular",
-  },
-  globals: {
-    "ts-jest": {
-      tsconfig: "./tsconfig.spec.json",
-      stringifyContentPathRegex: "\\.html$",
-      astTransformers: ["jest-preset-angular/InlineHtmlStripStylesTransformer"],
-      isolatedModules: true,
-      preserveSymlinks: true,
-    },
+    "^.+\\.(ts|js|html|tsx)$": [
+      "jest-preset-angular",
+      {
+        tsconfig: "./tsconfig.spec.json",
+        stringifyContentPathRegex: "\\.html$",
+        astTransformers: [
+          "jest-preset-angular/InlineHtmlStripStylesTransformer",
+        ],
+        isolatedModules: true,
+        preserveSymlinks: true,
+      },
+    ],
   },
   moduleFileExtensions: ["ts", "html", "js", "json"],
   moduleDirectories: ["node_modules", "src"],
